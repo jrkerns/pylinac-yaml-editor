531d160629b4638dda39be77ee91c4a185cd6588
James Kerns <jkerns@radformation.com>
2024-09-26 16:27:58 -0500

    test simple JS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
 import * as monaco from 'monaco-editor';
 
+self.MonacoEnvironment = {
+	getWorkerUrl: function (moduleId, label) {
+		// if (label === 'json') {
+		// 	return './json.worker.bundle.js';
+		// }
+		// if (label === 'css' || label === 'scss' || label === 'less') {
+		// 	return './css.worker.bundle.js';
+		// }
+		// if (label === 'html' || label === 'handlebars' || label === 'razor') {
+		// 	return './html.worker.bundle.js';
+		// }
+		// if (label === 'typescript' || label === 'javascript') {
+		// 	return './ts.worker.bundle.js';
+		// }
+		return './editor.worker.bundle.js';
+	}
+};
+
 monaco.editor.create(document.getElementById('editor'), {
-	value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-	language: 'javascript'
+	value: [
+		'from banana import *',
+		'',
+		'class Monkey:',
+		'	# Bananas the monkey can eat.',
+		'	capacity = 10',
+		'	def eat(self, N):',
+		"		'''Make the monkey eat N bananas!'''",
+		'		capacity = capacity - N*banana.size',
+		'',
+		'	def feeding_frenzy(self):',
+		'		eat(9.25)',
+		'		return "Yum yum"'
+	].join('\n'),
+	language: 'python'
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 // webpack.config.js
 const path = require('path');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+	entry: {
+		app: './src/index.js',
+		'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+		// 'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
+		// 'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
+		// 'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
+		// 'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
+	},
   output: {
-    filename: 'bundle.js',
+    globalObject: "self",
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/pylinac-yaml-editor', // Ensures proper path resolution
+    clean: true
   },
   module: {
     rules: [
@@ -26,8 +36,12 @@ module.exports = {
   },
   mode: 'development', // Set to development for better debugging
   plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        inject: true
+      }),
     new MonacoWebpackPlugin({
-      languages: ['yaml'],
+      languages: ['python'],
       // Add any other languages or features you need
     }),
   ],
@@ -40,6 +54,7 @@ module.exports = {
     open: true, // Automatically open the browser
     hot: true, // Enable hot module replacement
     historyApiFallback: true, // For SPAs, ensures that the index.html is served for any 404 responses
+    watchFiles: ['*.html'], // Watch for changes in HTML files
   },
   devtool: 'inline-source-map', // Useful for debugging
 };
